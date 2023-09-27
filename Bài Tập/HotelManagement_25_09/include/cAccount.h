f86fdb72eac4a8e4d6751996698f34797dd4441d
tienalex97 <141719318+tienalex97@users.noreply.github.com>
2023-09-27 10:41:04 +0700

    Create cAccount.h

---
+++ Bài Tập/HotelManagement_25_09/include/cAccount.h
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+typedef enum
+{
+    CLIENT, 
+    MANAGER
+} Authorization;
+
+
+class cAccount
+{
+    private:    
+        string sUserName;
+        string sUserPassword;
+        int nAuthorization;
+        int ID;
+        static int id;
+    public:
+        cAccount(string username, string password);
+        string GetUserName();
+        string GetPassWord();
+        int GetAuthorization();
+
+        void SetAuthorization(int authorization);
+
+};
